9a0a4c48dc7a8cd4b35c00c76ec9ef7dc77adb9f
al-momen663 <al.momen663@gmail.com>
2026-02-02 21:52:51 +0600

    Inidial Project Build

---
+++ src/main/java/com/project/SharePlate/SharePlateApplication.java
@@ -0,0 +1,13 @@
+package com.project.SharePlate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharePlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharePlateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/SharePlate/SharePlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SharePlate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharePlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
