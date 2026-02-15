bc4265ec736c49c6258f1dd870e326c1d1271454
al-momen663 <al.momen663@gmail.com>
2026-02-15 11:30:13 +0600

    icon featched through url

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -5,3 +5,10 @@ import { db } from '../lib/firebase';
 import { User, UserRole, Donation, DonationStatus, DonationType } from '../types';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
