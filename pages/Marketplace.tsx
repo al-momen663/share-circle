b4d785135570ae5a0c5ab770055782039525d092
Yeasmin Akter <yeasminakter0571.bd@gmail.com>
2026-03-02 23:28:30 +0600

    add loading spinner state

--- pages/Marketplace.tsx
+++ pages/Marketplace.tsx
@@ -93,3 +93,9 @@ const Marketplace: React.FC<MarketplaceProps> = ({ user }) => {
             </button>
           ))}
         </div>
+        
+        {loading ? (
+  <div className="flex justify-center py-20">
+    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-emerald-600"></div>
+  </div>
+) :
