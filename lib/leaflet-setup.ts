26d2c6ad11baf15d24217e277134aee3286b1561
al-momen663 <al.momen663@gmail.com>
2026-04-06 21:00:53 +0600

    leaflet added

---
+++ lib/leaflet-setup.ts
@@ -0,0 +1,13 @@
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix Leaflet default icon issue
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
+// Export L for convenience if needed
+export default L;
