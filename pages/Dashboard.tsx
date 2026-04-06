ae61a48e3fee8ed8ff0b23ad28b70e39cf5de78a
al-momen663 <al.momen663@gmail.com>
2026-04-06 20:59:37 +0600

    dashboard updated

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -10,14 +10,6 @@ import { formatLocation } from '../lib/utils';
 
 import LocationSearch from '../components/LocationSearch';
 
-// Fix Leaflet icon issue
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-});
-
 const foodIcon = new L.Icon({
     iconUrl: 'https://cdn-icons-png.flaticon.com/512/3194/3194591.png',
     iconSize: [35, 35],
