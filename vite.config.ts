55c570063630825e53814e9550e41a07fb7c8917
al-momen663 <al.momen663@gmail.com>
2026-04-06 20:25:06 +0600

    vite configured

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     port: 3000,
     host: '0.0.0.0',
   },
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
